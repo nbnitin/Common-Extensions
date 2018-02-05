525019f6e51ac7c4d90db7c8bf58a6cc10c4de76
nbnitin <nbnitin.bhatia@gmail.com>
2018-02-05 15:38:35 +0530

    Update ViewExtension.swift

--- ViewExtension.swift
+++ ViewExtension.swift
@@ -29,6 +29,28 @@ extension UIView{
         case DEFAULT = 3
     }
     
+    enum VerticalLocation: String {
+        case bottom
+        case top
+    }
+
+    func addShadow(location: VerticalLocation, color: UIColor = .black, opacity: Float = 0.3, radius: CGFloat = 5.0) {
+        switch location {
+        case .bottom:
+            addShadow(offset: CGSize(width: 0, height: 10), color: color, opacity: opacity, radius: radius)
+        case .top:
+            addShadow(offset: CGSize(width: 0, height: -1), color: color, opacity: opacity, radius: radius)
+        }
+    }
+    
+    func addShadow(offset: CGSize, color: UIColor = .black, opacity: Float = 0.5, radius: CGFloat = 5.0) {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowRadius = radius
+    }
+    
     
     func addBottomBorder(){
         let view:UIView = self
