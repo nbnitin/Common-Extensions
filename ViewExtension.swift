74f5691f576107be7a24ad49c92e70c6eed576e1
nbnitin <nbnitin.bhatia@gmail.com>
2018-02-14 19:39:26 +0530

    Update ViewExtension.swift

--- ViewExtension.swift
+++ ViewExtension.swift
@@ -258,3 +258,21 @@ extension UILabel{
         
     }
 }
+
+extension UICollectionViewCell{
+    func addAllCornerRadiusToCell(radius: CGFloat) {
+        self.contentView.layer.cornerRadius = radius
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+    }
+    
+    func addShadow(color:UIColor=UIColor.lightGray){
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = CGSize(width:0,height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false;
+        self.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
+
+    }
