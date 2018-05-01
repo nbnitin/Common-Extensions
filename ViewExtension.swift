5d2f41a1b0b8a849a553839e790888850c6d67cb
Nitin Bhatia <nbnitin.bhatia@gmail.com>
2018-05-01 14:39:33 +0530

    Update ViewExtension.swift

--- ViewExtension.swift
+++ ViewExtension.swift
@@ -277,6 +277,43 @@ extension UICollectionViewCell{
 
     }
     
+    extension UIColor {
+    public convenience init?(hexString: String,alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+       
+        if ( hexString.hasPrefix("#") ) {
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    convenience init(hex:Int, alpha:CGFloat = 1.0) {
+        self.init(
+            red:   CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8)  / 255.0,
+            blue:  CGFloat((hex & 0x0000FF) >> 0)  / 255.0,
+            alpha: alpha
+        )
+    }
+}/*Usage  var likedColor = ""
+        if ( articles[indexPath.row].isUserLiked ) {
+            likedColor = "#ff0000"
+        } else {
+            likedColor = "#252525"
+        }
+    UIColor(hexString: likedColor)
+    */
     
   extension UIImage{
     enum assetIdentifier : String{
